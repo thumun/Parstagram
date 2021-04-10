78b19fa000b137866cfc1e60b98c29654b566f48
nehathumu <nthumu@haverford.edu>
2021-04-09 21:58:53 -0400

    adding files

--- app/src/main/java/com/example/parstagram/Post.java
+++ app/src/main/java/com/example/parstagram/Post.java
@@ -5,12 +5,15 @@ import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
+import java.util.Date;
+
 @ParseClassName("Post")
 public class Post extends ParseObject {
 
     public static final String KEY_DESCRIPTION = "description";
     public static final String KEY_IMAGE = "image";
     public static final String KEY_USER = "user";
+    public static final String KEY_CREATED_KEY = "createdAt";
 
     public String getDescription(){
         return getString(KEY_DESCRIPTION);
@@ -35,4 +38,6 @@ public class Post extends ParseObject {
     public void setUser(ParseUser user){
         put(KEY_USER, user);
     }
+
+    public Date getCreatedAt() {return getDate(KEY_CREATED_KEY);}
 }
